2f58aaa9cbcf9e543a6f217e7a6499a4452f21a6
Marcello Marques <oliv.marq.marcello@gmail.com>
2020-07-02 18:58:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,81 @@
-const waitingForHeader = setInterval(() => {
-  const header = document.querySelector('._1QUKR');
-  if (header) {
-    var speed = 1.00;
-    clearInterval(waitingForHeader)
+const store = {
+  keyboardActive: false,
+  speed: 1.00
+};
 
-    const speedContainer = document.createElement("div");
-    speedContainer.classList.add("speedContainer");
-    header.appendChild(speedContainer)
-
-    const minusButton = document.createElement("button")
-    minusButton.innerHTML = "-"
-    minusButton.classList.add("speedButton")
-    speedContainer.appendChild(minusButton)
-
-    const speedDisplay = document.createElement("span");
-    speedDisplay.innerHTML = speed.toFixed(2);
-    speedDisplay.classList.add("speedDisplay");
-    speedContainer.appendChild(speedDisplay)
-
-    const plusButton = document.createElement("button")
-    plusButton.innerHTML = "+"
-    plusButton.classList.add("speedButton")
-    speedContainer.appendChild(plusButton)
-
-    minusButton.addEventListener("click", () => {
-      decreaseSpeed()
-    })
-
-    plusButton.addEventListener("click", () => {
-      increaseSpeed()
-    })
-
-    function decreaseSpeed() {
-      if (speed > 0) {
-        speed = speed - 0.25;
-        speedDisplay.innerHTML = speed.toFixed(2);
-        updateSpeed(speed);
+const stateHandler = {
+  set(target, property, value, _receiver) {
+    if (property === 'speed') {
+      if (value < 0.1) {
+        value = 0.1;
       }
+      const display = document.querySelector('.speed-display');
+      display.innerHTML = value.toFixed(2);
     }
-
-    function increaseSpeed() {
-      speed = speed + 0.25;
-      speedDisplay.innerHTML = speed.toFixed(2);
-      updateSpeed(speed);
-    }
-
-    function updateSpeed(speed) {
-      let audios = document.querySelectorAll("audio");
-      audios.forEach(audio => {
-        audio.playbackRate = speed;
-      })
-    }
-
+    return Reflect.set(target, property, value);
   }
+};
 
+const state = new Proxy(store, stateHandler);
+
+const handleDocumentClick = (event, $headers) => {
+  const path = event.path || (event.composedPath && event.composedPath());
+  state.keyboardActive = false;
+
+  ($headers || []).forEach(($header) => {
+    state.keyboardActive |= path.includes($header);
+  });
+
+  if (event.target.getAttribute('data-testid') === "audio-play") {
+    (document.querySelectorAll('audio') || [])
+      .forEach($audio => $audio.playbackRate = state.speed);
+  }
+}
+
+const handleDocumentKeyPress = (event) => {
+  if (state.keyboardActive) {
+    if (event.code === 'KeyD') state.speed += 0.1;
+    if (event.code === 'KeyS') state.speed -= 0.1;
+  }
+}
+
+const createUI = ($root) => {
+  const $speedContainer = document.createElement("div");
+  $speedContainer.classList.add("speed-container");
+  $root.appendChild($speedContainer)
+
+  const $minusButton = document.createElement("button")
+  $minusButton.innerHTML = "-"
+  $minusButton.classList.add("speed-button")
+  $speedContainer.appendChild($minusButton)
+
+  const $speedDisplay = document.createElement("span");
+  $speedDisplay.innerHTML = state.speed.toFixed(2);
+  $speedDisplay.classList.add("speed-display");
+  $speedContainer.appendChild($speedDisplay)
+
+  const $plusButton = document.createElement("button")
+  $plusButton.innerHTML = "+"
+  $plusButton.classList.add("speed-button")
+  $speedContainer.appendChild($plusButton)
+
+  $minusButton.addEventListener("click", () =>  state.speed -= 0.1)
+  $plusButton.addEventListener("click", () => state.speed += 0.1)
+}
+
+
+let attempts = 10;
+const waitLoading = setInterval(() => {
+  const $headers = document.querySelectorAll('header') || [];
+  const $header = $headers[0];
+  if ($header) {
+    clearInterval(waitLoading)
+    createUI($header);
+    document.addEventListener('click', event => handleDocumentClick(event, $headers));
+    document.addEventListener('keypress', handleDocumentKeyPress);
+  }
+  attempts--;
+  if (!attempts) {
+    clearInterval(waitLoading);
+  }
 }, 1000)
-
-
-
-
-
